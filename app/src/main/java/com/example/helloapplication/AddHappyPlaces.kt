96460387fd0aea5b473aa40730d03b6785617212
Lin <omasajiri@gmail.com>
2021-07-27 13:27:31 +0100

    feat: settup permissions

--- app/src/main/java/com/example/helloapplication/AddHappyPlaces.kt
+++ app/src/main/java/com/example/helloapplication/AddHappyPlaces.kt
@@ -1,15 +1,27 @@
 package com.example.helloapplication
 
+import android.Manifest
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.example.helloapplication.databinding.ActivityAddHappyPlacesBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddHappyPlaces : AppCompatActivity() {
-    lateinit var binding: ActivityAddHappyPlacesBinding
+    private lateinit var binding: ActivityAddHappyPlacesBinding
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +34,7 @@ class AddHappyPlaces : AppCompatActivity() {
             onBackPressed()
         }
 
-        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
             cal.set(Calendar.DAY_OF_YEAR, year)
             cal.set(Calendar.MONTH, month)
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
@@ -41,11 +53,72 @@ class AddHappyPlaces : AppCompatActivity() {
                     ).show()
         }
 
+
+        binding.tvAddImage.setOnClickListener {
+            val pictureDialog = AlertDialog.Builder(this)
+            pictureDialog.setTitle("Select Action")
+            val pictureDialogItems = arrayOf("Select photo from gallery", "Capture photo from camera" )
+            pictureDialog.setItems( pictureDialogItems ){
+                _, which ->
+                when(which){
+                    0 -> choosePhotoFromGallery()
+                    1 -> Toast.makeText(this,
+                        "camera coming soon.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            pictureDialog.show()
+        }
+
     }
 
     private fun updateDateInView(){
         val myFormat = "dd/MM/yyyy"
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
         binding.etDate.setText(sdf.format(cal.time).toString())
+    }
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object: MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()){
+                        Toast.makeText(this@AddHappyPlaces,
+                            "Storage READ/WRITE permission are granted. Now you can select an image from GALLERY or lets says phone storage.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread()
+            .check()
+    }
+
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"){
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
